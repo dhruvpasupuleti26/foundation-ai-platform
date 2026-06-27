fe3ac7367a41bb06756aec2036f3d102d5fc68b7
dhruvpasupuleti26 <dhruvsp@iisc.ac.in>
2026-06-27 11:55:53 +0530

    removed unnecessary files

--- test_docker_manager.py
+++
@@ -1,9 +0,0 @@
-from llm_platform.serving.docker_manager import DockerManager # import class def from llm_platform/serving/docker_manager.py
-
-print("Starting test.")
-
-manager = DockerManager(config_path="configs/vllm_config.yaml")
-
-print("Automated container launch will be done shortly.")
-
-manager.start_vllm_server()
